4c1a866bca215c6a81d25e0fde211eb4dce654a5
neeldarji12 <neeldarji1431@gmail.com>
2026-02-21 09:51:35 +0530

    Set up Firebase configuration and auth
    Initialize Firebase app and authentication.

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,1 +1,17 @@
 
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "shreya-e2366.firebaseapp.com",
+  projectId: "shreya-e2366",
+  storageBucket: "shreya-e2366.firebasestorage.app",
+  messagingSenderId: "453305684635",
+  appId: "1:453305684635:web:8132198d50a0fdd315ac3e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
